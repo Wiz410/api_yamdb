0344996ff209a569f8b40efd07866014782d9b0d
Данила Полунин <d.polunin2001@gmail.com>
2023-09-07 15:58:03 +0300

    Edit user, token, permission, add v1.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,63 +1,7 @@
 from django.urls import path, include
-from rest_framework import routers
 
-from .views import (
-    CategoriesViewSet,
-    GenresViewSet,
-    TitlesViewSet,
-    CommentsViewSet,
-    ReviewViewSet,
-    UsersViewSet,
-    APISingUp,
-    APIToken
-)
-
-v1_router = routers.DefaultRouter()
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='reviews'
-)
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentsViewSet,
-    basename='comments'
-)
-v1_router.register(
-    r'users',
-    UsersViewSet,
-    basename='users'
-)
-v1_router.register(
-    r'categories',
-    CategoriesViewSet,
-    basename='categories'
-)
-v1_router.register(
-    r'genres',
-    GenresViewSet,
-    basename='genres'
-)
-v1_router.register(
-    r'titles',
-    TitlesViewSet,
-    basename='titles'
-)
-
-auth_patterns = [
-    path(
-        'signup/',
-        APISingUp.as_view(),
-        name='signup'
-    ),
-    path(
-        'token/',
-        APIToken.as_view(),
-        name='token'
-    )
-]
+from .v1 import urls as v1_urls
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/', include(auth_patterns)),
+    path('v1/', include(v1_urls)),
 ]
--- api_yamdb/api/v1/permissions.py
+++ api_yamdb/api/v1/permissions.py
@@ -12,7 +12,7 @@ class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view) -> bool:
         return (
             request.user.is_authenticated
-            and request.user.role == 'admin'
+            and request.user.is_admin
             or request.user.is_superuser
         )
 
@@ -21,7 +21,7 @@ class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
-                and request.user.role == 'admin')
+                and request.user.is_admin)
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
@@ -29,7 +29,7 @@ class AdminOrReadOnly(permissions.BasePermission):
 
         return (
             request.user.is_authenticated
-            and (request.user.role == 'admin'
+            and (request.user.is_admin
                  or request.user.is_superuser)
         )
 
@@ -50,8 +50,8 @@ class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-            or request.user.role == 'moderator'
-            or request.user.role == 'admin' or request.user.is_superuser
+            or request.user.is_moderator
+            or request.user.is_admin or request.user.is_superuser
             or (
                 request.method == 'POST'
                 and request.user.is_authenticated
